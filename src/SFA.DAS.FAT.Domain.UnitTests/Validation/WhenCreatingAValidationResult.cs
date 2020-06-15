dce7a5390c60a39432d08a192e10aae7c8fea341
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-15 12:47:50 +0100

    Add ValidationResult class and tests

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Validation/WhenCreatingAValidationResult.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Validation;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Validation
+{
+    public class WhenCreatingAValidationResult
+    {
+        [Test]
+        public void Then_If_There_Are_No_Values_In_The_Dictionary_It_Is_Valid()
+        {
+            //Arrange Act
+            var actual = new ValidationResult();
+            
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+
+        [Test]
+        public void Then_If_There_Are_Values_In_The_Dictionary_It_Is_Not_Valid()
+        {
+            //Arrange
+            var actual = new ValidationResult();
+            
+            //Act
+            actual.AddError("test","test");
+            
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+        }
+
+        [Test]
+        public void Then_The_Message_Is_Automatically_Created_From_The_PropertyName()
+        {
+            //Arrange
+            var actual = new ValidationResult();
+            
+            //Act
+            actual.AddError("test");
+            
+            //Assert
+            Assert.AreEqual("test has not been supplied", actual.ValidationDictionary.First().Value); 
+        }
+    }
+}

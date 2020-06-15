b375c3edda9bb2a2d6bc97d4625ffeb49e001a01
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-15 16:15:12 +0100

    Add GetCourseApiRequest
    Added GetCourseApiRequest and test for constructing the GetUrl

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCourseApiRequest.cs
@@ -0,0 +1,20 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Courses.Api;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Courses.Api
+{
+    public class WhenCreatingTheGetCourseApiRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Get_Url_Is_Constructed_Correctly(string baseUrl, int id)
+        {
+            //Arrange Act
+            var actual = new GetCourseApiRequest(baseUrl, id);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}findapprenticeshiptraining/trainingcourses/{id}");
+        }
+    }
+}

85b4008877b3ef705ef83d5ac3eb41e840bfea2e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-15 15:57:15 +0100

    Add ApiClient
    Added ApiClient and Get request for calling APIM. Subscription key is always added. The url is taken from the request and response mapped back from the Generic response requested

---
+++ src/SFA.DAS.FAT.Domain/Configuration/FindApprenticeshipTrainingConfiguration.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.FAT.Domain.Configuration
+{
+    public class FindApprenticeshipTrainingConfiguration
+    {
+        public string ApiKey { get ; set ; }
+    }
+}
---
+++ src/SFA.DAS.FAT.Domain/Interfaces/IApiClient.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FAT.Domain.Interfaces
+{
+    public interface IApiClient
+    {
+        Task<TResponse> Get<TResponse>(IGetApiRequest request);
+    }
+}

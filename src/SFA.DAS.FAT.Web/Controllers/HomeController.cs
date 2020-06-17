6a9e59fad5a28def7f8df72c807bbc01c2289de0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-17 13:25:25 +0100

    Update home route name

--- src/SFA.DAS.FAT.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.FAT.Web/Controllers/HomeController.cs
@@ -3,10 +3,10 @@ using SFA.DAS.FAT.Web.Infrastructure;
 
 namespace SFA.DAS.FAT.Web.Controllers
 {
-    [Route("service")]
+    [Route("Home")]
     public class HomeController : Controller
     {
-        [Route("",Name = RouteNames.ServiceStart)]
+        [Route("Index",Name = RouteNames.ServiceStart)]
         public IActionResult Index()
         {
             return View();

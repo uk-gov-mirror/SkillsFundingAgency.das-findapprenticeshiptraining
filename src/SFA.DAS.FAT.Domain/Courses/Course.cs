3e6ee1510ff2f17f2952eca55819d27ad7bd03db
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-18 11:39:39 +0100

    Correct property name for core skills

--- src/SFA.DAS.FAT.Domain/Courses/Course.cs
+++ src/SFA.DAS.FAT.Domain/Courses/Course.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.FAT.Domain.Courses
         public string OverviewOfRole { get; set; }
         public string Route { get; set; }
         public string IntegratedDegree { get; set; }
-        public string CoreSkills { get ; set ; }
+        public string CoreSkillsCount { get ; set ; }
         public string TypicalJobTitles { get ; set ; }
         public string StandardPageUrl { get ; set ; }
         public int TypicalDuration { get ; set ; }

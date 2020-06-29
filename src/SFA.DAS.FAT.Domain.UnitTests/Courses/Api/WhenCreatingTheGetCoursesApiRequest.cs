1c6ac9b2d0d0f061a59ea8ac05054b566c100590
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-29 08:57:19 +0100

    Update GetCoursesApiRequest to take routeIds

--- src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCoursesApiRequest.cs
+++ src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCoursesApiRequest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
@@ -8,7 +10,17 @@ namespace SFA.DAS.FAT.Domain.UnitTests.Courses.Api
     public class WhenCreatingTheGetCoursesApiRequest
     {
         [Test, AutoData]
-        public void Then_The_Get_Url_Is_Constructed_Correctly(string baseUrl, string keyword)
+        public void Then_The_Get_Url_Is_Constructed_Correctly(string baseUrl, string keyword, List<Guid> sectors)
+        {
+            //Arrange Act
+            var actual = new GetCoursesApiRequest(baseUrl, keyword, sectors);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}trainingcourses?keyword={keyword}&routeIds={string.Join("&routeIds=", sectors)}");
+        }
+
+        [Test, AutoData]
+        public void Then_If_The_List_Of_Sectors_Is_Null_The_Url_Is_Constructed_Correctly(string baseUrl, string keyword)
         {
             //Arrange Act
             var actual = new GetCoursesApiRequest(baseUrl, keyword);

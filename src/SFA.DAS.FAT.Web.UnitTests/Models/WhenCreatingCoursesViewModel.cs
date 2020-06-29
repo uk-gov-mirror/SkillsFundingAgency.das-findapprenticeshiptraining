8792dfb55788b72c779365aa91430beb02c784a3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-29 13:38:14 +0100

    Change CoursesViewModel to build clear links

--- src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingCoursesViewModel.cs
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingCoursesViewModel.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Courses;
 using SFA.DAS.FAT.Web.Models;
 
 namespace SFA.DAS.FAT.Web.UnitTests.Models
@@ -22,6 +28,72 @@ namespace SFA.DAS.FAT.Web.UnitTests.Models
             };
 
             viewModel.TotalMessage.Should().Be(expectedMessage);
+        }
+
+        [Test, AutoData]
+        public void Then_No_Filter_Items_Builds_Correct_Clear_Links()
+        {
+            //Arrange
+            var viewModel = new CoursesViewModel();
+            
+            //Assert
+            viewModel.ClearKeywordLink.Should().BeEmpty();
+            viewModel.ClearSectorLinks.Should().BeEmpty();
+        }
+
+        [Test, AutoData]
+        public void Then_The_Clear_Keyword_Link_Is_Generated_If_Filtered_By_Keyword_With_Sectors(List<Guid> selectedRoutes, string keyword)
+        {
+            //Arrange Act
+            var model = BuildCoursesViewModel(selectedRoutes, keyword);
+
+            //Assert
+            Assert.IsNotNull(model.ClearKeywordLink);
+            Assert.AreEqual("?sectors=" + string.Join("&sectors=", model.SelectedSectors), model.ClearKeywordLink);
+        }
+        
+        [Test, AutoData]
+        public void Then_The_Clear_Filter_Items_Are_Built_From_The_Selected_Items(List<Guid> selectedRoutes, string keyword)
+        {
+            //Arrange Act
+            var model = BuildCoursesViewModel(selectedRoutes, keyword);
+
+            //Assert
+            var clearLinkCount = selectedRoutes.Count;
+            Assert.AreEqual(clearLinkCount,model.ClearSectorLinks.Count);
+            
+            foreach (var selectedRoute in selectedRoutes)
+            {
+                var sector = model.Sectors.SingleOrDefault(c => c.Id.Equals(selectedRoute));
+                
+                Assert.IsNotNull(sector);
+                Assert.IsTrue(model.ClearSectorLinks.ContainsKey(sector.Route));
+                Assert.AreEqual(clearLinkCount-1, model.ClearSectorLinks.Count(c=>c.Value.Contains($"sectors={selectedRoute}")));    
+                Assert.AreEqual(clearLinkCount, model.ClearSectorLinks.Count(c=>c.Value.Contains($"keyword={keyword}")));    
+            }
+
+            
+        }
+
+        private static CoursesViewModel BuildCoursesViewModel(List<Guid> selectedRoutes, string keyword)
+        {
+            var fixture = new Fixture();
+            var sectors = selectedRoutes
+                .Select(selectedRoute => new SectorViewModel(
+                    new Sector
+                    {
+                        Id = selectedRoute,
+                        Route = fixture.Create<string>()
+                    }, null))
+                .ToList();
+
+            var model = new CoursesViewModel
+            {
+                Sectors = sectors,
+                Keyword = keyword,
+                SelectedSectors = selectedRoutes,
+            };
+            return model;
         }
     }
 }
--- src/SFA.DAS.FAT.Web/Models/CoursesViewModel.cs
+++ src/SFA.DAS.FAT.Web/Models/CoursesViewModel.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using SFA.DAS.FAT.Domain.Courses;
 
 namespace SFA.DAS.FAT.Web.Models
 {
@@ -12,13 +12,50 @@ namespace SFA.DAS.FAT.Web.Models
         public int TotalFiltered { get ; set ; }
         public List<SectorViewModel> Sectors { get ; set ; }
         public string TotalMessage => GetTotalMessage();
-
+        public List<Guid> SelectedSectors { get ; set ; }
+        public Dictionary<string, string> ClearSectorLinks => BuildClearSelectedFilterLink();
+        public string ClearKeywordLink => BuildClearKeywordFilterLink();
+        
         private string GetTotalMessage()
         {
             var totalToUse = string.IsNullOrEmpty(Keyword) ? Total : TotalFiltered;
 
             return $"{totalToUse} result" + (totalToUse!=1 ? "s": "");
         }
+
+        private string BuildClearKeywordFilterLink()
+        {
+            return SelectedSectors != null ? "?sectors=" + string.Join("&sectors=", SelectedSectors) : "";
+        }
+
+        private Dictionary<string, string> BuildClearSelectedFilterLink ( )
+        {
+            var clearFilterLinks = new Dictionary<string, string>();
+            if (SelectedSectors == null)
+            {
+                return clearFilterLinks;
+            }
+            
+            foreach (var selectedSector in SelectedSectors)
+            {
+                var clearFilterString = string.Empty;
+                var separator = "?";
+                if (!string.IsNullOrEmpty(Keyword))
+                {
+                    clearFilterString = $"?keyword={Keyword}";
+                    separator = "&";
+                }
+
+                clearFilterString += $"{separator}sectors=" + string.Join("&sectors=", SelectedSectors.Where(c => !c.Equals(selectedSector)));
+
+                var sector = Sectors.SingleOrDefault(c => c.Id.Equals(selectedSector));
+                
+                clearFilterLinks.Add(sector.Route, clearFilterString);
+            }
+
+            return clearFilterLinks;
+        }
+
     }
     
 }

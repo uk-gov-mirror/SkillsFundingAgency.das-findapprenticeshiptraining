1cd98a0e48e0b0009ac64d5cd56669aa8662a510
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-22 19:24:29 +0100

    Correct URL building and add missing test coverage

--- src/SFA.DAS.FAT.Domain/Courses/Api/GetCoursesApiRequest.cs
+++ src/SFA.DAS.FAT.Domain/Courses/Api/GetCoursesApiRequest.cs
@@ -27,7 +27,7 @@ namespace SFA.DAS.FAT.Domain.Courses.Api
             var url = $"{BaseUrl}trainingcourses?keyword={Keyword}";
             if (OrderBy != OrderBy.None)
             {
-                url += $"orderby={OrderBy}";
+                url += $"&orderby={OrderBy}";
             }
             if (Sectors != null && Sectors.Any())
             {

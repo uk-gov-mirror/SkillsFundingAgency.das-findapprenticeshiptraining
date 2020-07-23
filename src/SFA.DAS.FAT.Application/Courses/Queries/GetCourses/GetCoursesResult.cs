d0a471bc170a8bba11ced8c163d41d4381b34780
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-23 15:56:03 +0100

    Remove unused property

--- src/SFA.DAS.FAT.Application/Courses/Queries/GetCourses/GetCoursesResult.cs
+++ src/SFA.DAS.FAT.Application/Courses/Queries/GetCourses/GetCoursesResult.cs
@@ -10,6 +10,5 @@ namespace SFA.DAS.FAT.Application.Courses.Queries.GetCourses
         public List<Level> Levels { get; set; }
         public int TotalFiltered { get ; set ; }
         public int Total { get ; set ; }
-        public OrderBy OrderBy { get; set; }
     }
 }

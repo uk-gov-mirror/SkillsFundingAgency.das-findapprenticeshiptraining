204318df6d5dfdb2df0808c401b0e468fd4e5257
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-29 10:26:49 +0100

    Create Test file for Course Provider Details

---
+++ src/SFA.DAS.FAT.Web.UnitTests/Controllers/CoursesControllerTests/WhenGettingCourseProviderDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAT.Web.UnitTests.Controllers.CoursesControllerTests
+{
+    public class WhenGettingCourseProviderDetails
+    {
+
+        [Test, MoqAutoData]
+        public async Task Then_The_Query_Is_Sent_And_Provider_Detail_Retrieved_And_Shown()
+        {
+
+        }
+    }
+}

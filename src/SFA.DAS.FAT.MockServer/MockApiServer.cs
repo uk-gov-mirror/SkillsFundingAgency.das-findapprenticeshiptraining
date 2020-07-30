67c48495929b33f97e828e859b429c2b198ee8f2
Scott Wakefield <scott@expertcc.co.uk>
2020-07-30 10:01:44 +0100

    FAT2-99 added setup for tests to be able to call api.

---
+++ src/SFA.DAS.FAT.MockServer/MockApiServer.cs
@@ -0,0 +1,50 @@
+﻿using WireMock.Logging;
+using WireMock.Matchers;
+using WireMock.Net.StandAlone;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.FAT.MockServer
+{
+    public static class MockApiServer
+    {
+        public static IWireMockServer Start()
+        {
+            var settings = new WireMockServerSettings
+            {
+                Port = 5003,
+                Logger = new WireMockConsoleLogger()
+            };
+            
+            var server = StandAloneApp.Start(settings);
+            
+            server.Given(Request.Create().WithPath("/trainingcourses")
+                .UsingGet()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("courses.json"));
+
+            server.Given(Request.Create().WithPath("/trainingcourses/123")
+                .UsingGet()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("course.json"));
+
+            server.Given(Request.Create().WithPath("/trainingcourses/123/providers")
+                .UsingGet()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("course-providers.json"));
+
+            return server;
+        }
+    }
+}

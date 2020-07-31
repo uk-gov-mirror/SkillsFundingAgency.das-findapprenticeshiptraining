3304de5ddf4181e0e48c1dc322ae935dee5a81b5
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-31 15:40:17 +0100

    Test passes: WhenCreatingTheGetCourseProviderDetailsApiRequest and URL is built correct

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCourseProviderDetailsApiRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Courses.Api;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Courses.Api
+{
+    public class WhenCreatingTheGetCourseProviderDetailsApiRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Get_Url_Is_Constructed_Correctly(string baseUrl, int id)
+        {
+            //Arrange Act
+            var actual = new GetCourseProviderDetailsApiRequest(baseUrl, id);
+
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}trainingcourses/{id}/course-provider-details");
+
+        }
+    }
+}

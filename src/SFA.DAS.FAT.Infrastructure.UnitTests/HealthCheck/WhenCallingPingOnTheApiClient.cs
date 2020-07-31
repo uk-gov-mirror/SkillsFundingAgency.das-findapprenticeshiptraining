4bbe8cd77277d246fe67fd475cc272bc9d57fb29
Peter Williams <peter.david.williams.1991@gmail.com>
2020-07-31 11:53:03 +0100

    WIP missed unit test

--- src/SFA.DAS.FAT.Infrastructure.UnitTests/HealthCheck/WhenCallingPingOnTheApiClient.cs
+++ src/SFA.DAS.FAT.Infrastructure.UnitTests/HealthCheck/WhenCallingPingOnTheApiClient.cs
@@ -1,12 +1,18 @@
-﻿using System.Threading;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
+using FluentAssertions;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Configuration;
 using SFA.DAS.FAT.Domain.Interfaces;
 using SFA.DAS.FAT.Infrastructure.Api;
 using SFA.DAS.FAT.Infrastructure.HealthCheck;
+using SFA.DAS.FAT.Infrastructure.UnitTests.HttpMessageHandlerMock;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.FAT.Infrastructure.UnitTests.HealthCheck
@@ -23,6 +29,28 @@ namespace SFA.DAS.FAT.Infrastructure.UnitTests.HealthCheck
             await healthCheck.CheckHealthAsync(healthCheckContext, CancellationToken.None);
             //Assert
             client.Verify(x => x.Ping(), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_The_Endpoint_Is_Called_And_The_Status_is_Returned(
+            FindApprenticeshipTrainingApi config)
+        {
+            //Arrange
+            config.BaseUrl = "https://test.local";
+            var configMock = new Mock<IOptions<FindApprenticeshipTrainingApi>>();
+            configMock.Setup(x => x.Value).Returns(config);
+            var response = new HttpResponseMessage
+            {
+                Content = new StringContent(""),
+                StatusCode = HttpStatusCode.Accepted
+            };
+            var httpMessageHandler = MessageHandler.SetupMessageHandlerMock(response, "/ping", config.Key);
+            var client = new HttpClient(httpMessageHandler.Object);
+            var apiClient = new ApiClient(client, configMock.Object);
+            //Act
+            var actual = await apiClient.Ping();
+            //Assert
+            actual.Should().Be((int)HttpStatusCode.Accepted);
         }
 
         [Test, MoqAutoData]

8a4e848ff6038c68a785d026520ed5ef340c0abc
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-31 15:27:27 +0100

    Test passes x2 for WhenValidatingAGetCourseProviderDetailsQuery

--- src/SFA.DAS.FAT.Application.UnitTests/Courses/Queries/GetCourseProviderDetails/WhenValidatingAGetCourseProviderDetailsQuery.cs
+++ src/SFA.DAS.FAT.Application.UnitTests/Courses/Queries/GetCourseProviderDetails/WhenValidatingAGetCourseProviderDetailsQuery.cs
@@ -1,11 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.FAT.Application.Courses.Queries.GetProvider;
 
 namespace SFA.DAS.FAT.Application.UnitTests.Courses.Queries.GetCourseProviderDetails
 {
     public class WhenValidatingAGetCourseProviderDetailsQuery
     {
-        // private GetProviderQueryValidator _validator;
+        private GetCourseProviderDetailsQueryValidator _validator;
+        
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new GetCourseProviderDetailsQueryValidator();
+        }
+
+        [Test]
+        public async Task Then_Returns_Validation_Errors_If_Request_Is_Not_Valid()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetProviderQuery());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.IsTrue(actual.ValidationDictionary.ContainsKey(nameof(GetProviderQuery.ProviderId)));
+        }
+
+        [Test]
+        public async Task Then_Is_Valid_Is_Returned_If_All_Values_Are_Valid()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetProviderQuery { ProviderId = 10 });
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
     }
 }

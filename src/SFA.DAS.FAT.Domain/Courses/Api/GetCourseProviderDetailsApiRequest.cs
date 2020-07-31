be20355baf1594622757fa57d2342bd0998b6f4e
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-31 13:27:44 +0100

    Fill out GetCourseProviderDetailsApiRequest

--- src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseProviderDetailsApiRequest.cs
+++ src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseProviderDetailsApiRequest.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SFA.DAS.FAT.Domain.Interfaces;
 
 namespace SFA.DAS.FAT.Domain.Courses.Api
 {
-    public class GetCourseProviderDetailsApiRequest
+    public class GetCourseProviderDetailsApiRequest :  IGetApiRequest
     {
+        public GetCourseProviderDetailsApiRequest(string baseUrl, int id)
+        {
+            BaseUrl = baseUrl;
+            Id = id;
+        }
+        private int Id { get; }
+        public string BaseUrl { get; }
+        public string GetUrl => $"{BaseUrl}trainingcourses/{Id}";
     }
 }

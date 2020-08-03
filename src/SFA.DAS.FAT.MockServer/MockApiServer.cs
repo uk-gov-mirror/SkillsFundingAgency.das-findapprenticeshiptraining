b1e935e73600a87f167e9b866a68fe0fe41e21cd
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-03 14:05:04 +0100

    Correct wire mock matching

--- src/SFA.DAS.FAT.MockServer/MockApiServer.cs
+++ src/SFA.DAS.FAT.MockServer/MockApiServer.cs
@@ -1,4 +1,8 @@
-﻿using WireMock.Logging;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using WireMock.Logging;
 using WireMock.Matchers;
 using WireMock.Net.StandAlone;
 using WireMock.RequestBuilders;
@@ -28,7 +32,8 @@ namespace SFA.DAS.FAT.MockServer
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile("courses.json"));
 
-            server.Given(Request.Create().WithPath("/trainingcourses/123")
+            server.Given(Request.Create()
+                .WithPath(IsCourse)
                 .UsingGet()
             ).RespondWith(
                 Response.Create()
@@ -36,15 +41,47 @@ namespace SFA.DAS.FAT.MockServer
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile("course.json"));
 
-            server.Given(Request.Create().WithPath("/trainingcourses/123/providers")
+            server.Given(Request.Create().WithPath(IsListOfProviders)
                 .UsingGet()
             ).RespondWith(
                 Response.Create()
                     .WithStatusCode(200)
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile("course-providers.json"));
-
+            
+            server.Given(Request.Create().WithPath(IsCourseProvider)
+                .UsingGet()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("course-provider.json"));
             return server;
         }
+
+        private static bool IsCourse(string arg)
+        {
+            return !arg.Contains("providers", StringComparison.CurrentCultureIgnoreCase) 
+                   && Regex.IsMatch(arg, @"/trainingcourses/[0-9]*");
+        }
+        private static bool IsListOfProviders(string arg)
+        {
+            var partCount = arg.Split("/");
+
+            if (partCount.Length == 4 
+                && partCount[1].Equals("trainingcourses", StringComparison.CurrentCultureIgnoreCase) 
+                && partCount[3].Equals("providers", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return Regex.IsMatch(arg, @"/trainingcourses/[0-9]*/providers");
+            }
+            return false;
+            
+        }
+        
+        private static bool IsCourseProvider(string arg)
+        {
+            return Regex.IsMatch(arg, @"/trainingcourses/[0-9]*/providers/[0-9]*");
+        }
+
     }
 }

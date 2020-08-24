c5ec99334abc1cc39c1c06cb5237d08393ae9f69
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-24 09:20:29 +0100

    Add get location get request

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Locations/Api/WhenCreatingGetLocationsApiRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Locations.Api;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Locations.Api
+{
+    public class WhenCreatingGetLocationsApiRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl, string searchTerm)
+        {
+            //Arrange Act
+            var actual = new GetLocationsApiRequest(baseUrl, searchTerm);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}locations?searchTerm={searchTerm}");
+        }
+    }
+}
---
+++ src/SFA.DAS.FAT.Domain/Locations/Api/GetLocationsApiRequest.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.FAT.Domain.Interfaces;
+
+namespace SFA.DAS.FAT.Domain.Locations.Api
+{
+    public class GetLocationsApiRequest : IGetApiRequest
+    {
+        private readonly string _searchTerm;
+
+        public GetLocationsApiRequest(string baseUrl, string searchTerm)
+        {
+            BaseUrl = baseUrl;
+            _searchTerm = searchTerm;
+        }
+
+        public string BaseUrl { get; }
+        public string GetUrl => $"{BaseUrl}locations?searchTerm={_searchTerm}";
+    }
+}

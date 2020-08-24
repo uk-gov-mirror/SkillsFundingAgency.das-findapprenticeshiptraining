77ada7dba7c6332dac53a0467693b08ffe081d63
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-24 10:39:30 +0100

    Add location service to call api

---
+++ src/SFA.DAS.FAT.Domain/Interfaces/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.FAT.Domain.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<Domain.Locations.Locations> GetLocations(string searchTerm);
+    }
+}
---
+++ src/SFA.DAS.FAT.Domain/Locations/Locations.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.FAT.Domain.Locations
+{
+    public class Locations
+    {
+        [JsonProperty("locations")]
+        public List<LocationItem> LocationItems { get; set; }
+
+        public class LocationItem
+        {
+            [JsonProperty("location")]
+            public LocationPoint LocationPoint { get; set; }
+
+            [JsonProperty("localAuthorityName")]
+            public string LocalAuthorityName { get; set; }
+
+            [JsonProperty("locationName")]
+            public string LocationName { get; set; }
+
+            [JsonProperty("countyName")]
+            public string CountyName { get; set; }
+        }
+        
+        public class LocationPoint
+        {
+            [JsonProperty("geoPoint")]
+            public List<double> GeoPoint { get; set; }
+        }
+    }
+}

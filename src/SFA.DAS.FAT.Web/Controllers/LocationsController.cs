c158e873b59c8e152759d6809cd99c3d9b37d7b0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-24 14:48:15 +0100

    Add locations controller and js for location autocomplete

---
+++ src/SFA.DAS.FAT.Web/Controllers/LocationsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.FAT.Application.Locations.Queries.GetLocations;
+using SFA.DAS.FAT.Web.Models;
+
+namespace SFA.DAS.FAT.Web.Controllers
+{
+    [Route("[controller]")]
+    public class LocationsController : Controller
+    {
+        private readonly ILogger<CoursesController> _logger;
+        private readonly IMediator _mediator;
+
+        public LocationsController (
+            ILogger<CoursesController> logger,
+            IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Locations([FromQuery]string searchTerm)
+        {
+            var result = await _mediator.Send(new GetLocationsQuery
+            {
+                SearchTerm = searchTerm
+            });
+
+            var model = new LocationsViewModel
+            {
+                Locations = result.LocationItems.Select(c=>(LocationViewModel)c).ToList()
+            };
+            
+            return new JsonResult(model);
+        }
+    }
+}
--- src/SFA.DAS.FAT.Web/wwwroot/js/site.js
+++ src/SFA.DAS.FAT.Web/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var $keywordsInput = $('#search-location');
+
+if ($keywordsInput.length > 0) {
+
+    var container, apiUrl;
+
+    $keywordsInput.wrap('<div id="fat-autocomplete-container"></div>');
+    container = document.querySelector('#fat-autocomplete-container');
+        apiUrl = '/locations';
+
+    $(container).empty();
+    var getSuggestions = function (query, updateResults) {
+
+        var results = [];
+        $.ajax({
+            url: apiUrl,
+            type: "get",
+            dataType: 'json',
+            data: { searchTerm: query }
+
+        }).done(function (data) {
+            results = data.locations.map(function (r) {
+                return r.locationName;
+            });
+            
+            updateResults(results);
+        });
+    };
+
+    accessibleAutocomplete({
+        element: container,
+        id: 'Locations',
+        name: 'Locations',
+        displayMenu: 'overlay',
+        showNoOptionsFound: false,
+        minLength: 3,
+        source: getSuggestions,
+        placeholder: ""
+    });
+}

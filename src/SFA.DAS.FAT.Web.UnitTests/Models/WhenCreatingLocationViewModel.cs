2639946666bf62219fba0dafe40d4d7fa96f2e6b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-24 14:47:34 +0100

    Add location view model

---
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingLocationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Web.Models;
+
+namespace SFA.DAS.FAT.Web.UnitTests.Models
+{
+    public class WhenCreatingLocationViewModel
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(Domain.Locations.Locations.LocationItem source)
+        {
+            var actual = (LocationViewModel) source;
+
+            actual.LocationName.Should().Be($"{source.LocationName}, {source.LocalAuthorityName}");
+        }
+    }
+}

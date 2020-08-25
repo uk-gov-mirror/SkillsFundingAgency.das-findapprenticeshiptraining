ee094af287090393606db8af2b046d3ffd87d9ac
Scott Wakefield <scott@expertcc.co.uk>
2020-08-25 18:34:14 +0100

    FAT2-108 removed commented code.

--- src/SFA.DAS.FAT.MockServer/MockApiServer.cs
+++ src/SFA.DAS.FAT.MockServer/MockApiServer.cs
@@ -24,50 +24,6 @@ namespace SFA.DAS.FAT.MockServer
             
             var server = StandAloneApp.Start(settings);
             
-            /*server.Given(Request.Create().WithPath("/trainingcourses")
-                .UsingGet()
-            ).RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBodyFromFile("courses.json"));
-
-            server.Given(Request.Create()
-                .WithPath(IsCourse)
-                .UsingGet()
-            ).RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBodyFromFile("course.json"));
-
-            server.Given(Request.Create()
-                .WithPath(IsExpiredCourse)
-                .UsingGet()
-            ).RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBodyFromFile("course-expired.json"));
-
-            server.Given(Request.Create().WithPath(IsListOfProviders)
-                .UsingGet()
-            ).RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBodyFromFile("course-providers.json"));
-            
-            server.Given(Request.Create().WithPath(IsCourseProvider)
-                .UsingGet()
-            ).RespondWith(
-                Response.Create()
-                    .WithStatusCode(200)
-                    .WithHeader("Content-Type", "application/json")
-                    .WithBodyFromFile("course-provider.json"));*/
-
-            // gnu way
-
             server.Given(Request.Create().WithPath(s => Regex.IsMatch(s,"/trainingcourses/\\d+/providers/\\d+$"))
                 .UsingGet()
             ).RespondWith(
@@ -118,38 +74,5 @@ namespace SFA.DAS.FAT.MockServer
 
             return server;
         }
-
-        /*private static bool IsCourse(string arg)
-        {
-            return !arg.Contains("providers", StringComparison.CurrentCultureIgnoreCase) 
-                   && !Regex.IsMatch(arg, @"/trainingcourses/101") 
-                   && Regex.IsMatch(arg, @"/trainingcourses/[0-9]*");
-        }*/
-
-        /*private static bool IsExpiredCourse(string arg)
-        {
-            return !arg.Contains("providers", StringComparison.CurrentCultureIgnoreCase)
-                   && Regex.IsMatch(arg, @"/trainingcourses/101");
-        }*/
-
-        /*private static bool IsListOfProviders(string arg)
-        {
-            var partCount = arg.Split("/");
-
-            if (partCount.Length == 4 
-                && partCount[1].Equals("trainingcourses", StringComparison.CurrentCultureIgnoreCase) 
-                && partCount[3].Equals("providers", StringComparison.CurrentCultureIgnoreCase))
-            {
-                return Regex.IsMatch(arg, @"/trainingcourses/[0-9]#1#providers");
-            }
-            return false;
-            
-        }*/
-        
-        /*private static bool IsCourseProvider(string arg)
-        {
-            return Regex.IsMatch(arg, @"/trainingcourses/[0-9]#1#providers/[0-9]*");
-        }*/
-
     }
 }

fca074cadaf33283e476b9225221b6126f4cb6e0
robyoung91 <robertmatthewyoung@gmail.com>
2020-08-25 16:14:42 +0100

    Include acceptance tests and update packages

---
+++ src/SFA.DAS.FAT.Web.AcceptanceTests/Features/CourseDetail.feature
@@ -0,0 +1,10 @@
+﻿Feature: CourseDetail
+	As an employer
+	I want to see if providers delivering a course need to be regulated 
+	So that I know that I should choose a provider with the correct authorisation
+
+@mytag
+Scenario: Navigate to course detail page for regulated course
+	When I navigate to the following url: /courses/333
+	Then an http status code of 200 is returned
+    And the regulated occupation header and message is displayed
---
+++ src/SFA.DAS.FAT.Web.AcceptanceTests/Steps/CourseDetailSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using SFA.DAS.FAT.Domain.Courses;
+using SFA.DAS.FAT.MockServer;
+using SFA.DAS.FAT.Web.AcceptanceTests.Infrastructure;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.FAT.Web.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class CourseDetailSteps
+    {
+        private readonly ScenarioContext _context;
+
+        public CourseDetailSteps(ScenarioContext context)
+        {
+            _context = context;
+        }
+
+        [Then("the regulated occupation header and message is displayed")]
+        public async Task ThenThePageContentIncludesTheFollowing()
+        {
+            var response = _context.Get<HttpResponseMessage>(ContextKeys.HttpResponse);
+            var json = DataFileManager.GetFile("course-regulated.json");
+            var expectedApiResponse = JsonConvert.DeserializeObject<TrainingCourse>(json);
+
+            var actualContent = await response.Content.ReadAsStringAsync();
+
+            actualContent.Should().Contain("<h3 class=\"govuk-heading-m govuk-!-margin-bottom-2\">Regulated occupation</h3>");
+            actualContent.Should().Contain($"<p class=\"govuk-body\">{expectedApiResponse.Course.Title} (level {expectedApiResponse.Course.Level}) needs a training provider who is approved by the appropriate regulatory body.</p>");
+        }
+    }
+}

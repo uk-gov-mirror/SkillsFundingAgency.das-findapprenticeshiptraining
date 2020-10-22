f0fcb96ee990f4839173a82b2cfb6b5dafde17a8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-10-22 22:00:08 +0100

    Add extension for formatting distances

---
+++ src/SFA.DAS.FAT.Web/Extensions/DecimalExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.FAT.Web.Extensions
+{
+    public static class DecimalExtension
+    {
+        public static string FormatDistance(this decimal value)
+        {
+            var returnValue = value.ToString("F1");
+
+            returnValue = returnValue.TrimEnd('0');
+            returnValue = returnValue.TrimEnd('.');
+            return returnValue;
+        }
+    }
+}

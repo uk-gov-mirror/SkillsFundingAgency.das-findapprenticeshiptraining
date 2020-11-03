f13e1308d9499c159de39f1724ca1c8d613042a9
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-11-03 16:35:23 +0000

    Build Accessibility Controller

---
+++ src/SFA.DAS.FAT.Web/Controllers/AccessibilityController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.FAT.Web.Controllers
+{
+    public class AccessibilityController : Controller
+    {
+        public IActionResult AccessibilityStatement()
+        {
+            return View();
+        }
+    }
+}

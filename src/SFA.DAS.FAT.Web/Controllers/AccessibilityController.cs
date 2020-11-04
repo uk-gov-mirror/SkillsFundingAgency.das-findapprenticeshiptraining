7b630895e6acfca2e35c04ec837fbb0639d2d52e
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-11-04 17:34:31 +0000

    Add RouteName attribute to Accessibility Controller

--- src/SFA.DAS.FAT.Web/Controllers/AccessibilityController.cs
+++ src/SFA.DAS.FAT.Web/Controllers/AccessibilityController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FAT.Web.Infrastructure;
 
 namespace SFA.DAS.FAT.Web.Controllers
 {
+    [Route("controller")]
     public class AccessibilityController : Controller
     {
+        [Route("", Name = RouteNames.AccessibilityStatement)]
         public IActionResult AccessibilityStatement()
         {
             return View();

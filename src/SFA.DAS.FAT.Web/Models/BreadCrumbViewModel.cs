e882447e380ddfaa719c0792c27372e8bf34de5a
Scott Wakefield <scott@expertcc.co.uk>
2020-11-09 15:59:50 +0000

    FAT2-273 updated models to handle breadcrumb.

---
+++ src/SFA.DAS.FAT.Web/Models/BreadCrumbViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.FAT.Web.Models
+{
+    public class BreadCrumbViewModel
+    {
+        public BreadCrumbLevel Level { get; set; }
+        public string CourseId { get; set; }
+        public string CourseDescription { get; set; }
+        public Dictionary<string, string> ProvidersFilters { get; set; }
+        public string QueryString { get; set; }
+    }
+
+    public enum BreadCrumbLevel
+    {
+        Home,
+        Error,
+        CourseList,
+        CourseDetails,
+        CourseProvidersList,
+        CourseProviderDetails
+    }
+}

828e11720c3733884ad8320c275df43753ed935a
Peter Williams <peter.david.williams.1991@gmail.com>
2020-11-23 10:18:11 +0000

    Updated provider list page

--- src/SFA.DAS.FAT.Web/Models/ProviderViewModel.cs
+++ src/SFA.DAS.FAT.Web/Models/ProviderViewModel.cs
@@ -210,8 +210,8 @@ namespace SFA.DAS.FAT.Web.Models
             viewModel.IsAvailable =  source != default;
             viewModel.FormattedDistanceInMiles = source != default && deliveryModeType != DeliveryModeType.Workplace
                 ? source.DistanceInMiles.FormatDistance() == "1" 
-                    ? "(1 mile away)" 
-                    : $"({source.DistanceInMiles.FormatDistance()} miles away)"
+                    ? ": 1 mile away" 
+                    : $": {source.DistanceInMiles.FormatDistance()} miles away"
                 : null;
             viewModel.NationalText = source != default && 
                 source.National && deliveryModeType == DeliveryModeType.Workplace ? "(national)" : null;

6d4fe56148ebe9674ef83ee635096881c96d8878
Peter Williams <peter.david.williams.1991@gmail.com>
2020-11-26 13:11:25 +0000

    WIP

---
+++ src/SFA.DAS.FAT.Web/Models/GaData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.FAT.Web.Models
+{
+    public class GaData
+    {
+        public string DataLoaded { get; set; } = "dataLoaded";
+        public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
+
+        public string Location { get; set; }
+        public string Vpv { get; set; }
+
+    }
+}

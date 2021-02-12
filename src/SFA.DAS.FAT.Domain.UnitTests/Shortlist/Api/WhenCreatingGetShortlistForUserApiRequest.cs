75a5986798d1dd848f464a07591f17358fd1ed3c
Scott Wakefield <scott@expertcc.co.uk>
2021-02-12 13:05:31 +0000

    FAT2-230 adding api request.

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Shortlist/Api/WhenCreatingGetShortlistForUserApiRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Shortlist.Api;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Shortlist.Api
+{
+    public class WhenCreatingGetShortlistForUserApiRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl, Guid shortlistUserId)
+        {
+            //Arrange Act
+            var actual = new GetShortlistForUserApiRequest(baseUrl, shortlistUserId);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}shortlist/users/{shortlistUserId}");
+        }
+    }
+}
---
+++ src/SFA.DAS.FAT.Domain/Shortlist/Api/GetShortlistForUserApiRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFA.DAS.FAT.Domain.Interfaces;
+
+namespace SFA.DAS.FAT.Domain.Shortlist.Api
+{
+    public class GetShortlistForUserApiRequest : IGetApiRequest
+    {
+        private readonly Guid _shortlistUserId;
+
+        public GetShortlistForUserApiRequest(string baseUrl, Guid shortlistUserId)
+        {
+            _shortlistUserId = shortlistUserId;
+            BaseUrl = baseUrl;
+        }
+
+        public string BaseUrl { get; }
+        public string GetUrl => $"{BaseUrl}shortlist/users/{_shortlistUserId}";
+    }
+}

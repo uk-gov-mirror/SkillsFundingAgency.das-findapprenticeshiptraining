4470c7ff4133153f8e36503ba56b3efd8a8d6de1
Scott Wakefield <scott@expertcc.co.uk>
2021-02-12 14:13:22 +0000

    FAT2-230 adding service.

--- src/SFA.DAS.FAT.Application.UnitTests/Shortlist/Services/WhenGettingShortlistForUser.cs
+++ src/SFA.DAS.FAT.Application.UnitTests/Shortlist/Services/WhenGettingShortlistForUser.cs
@@ -1,7 +1,36 @@
-﻿namespace SFA.DAS.FAT.Application.UnitTests.Shortlist.Services
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FAT.Application.Shortlist.Services;
+using SFA.DAS.FAT.Domain.Interfaces;
+using SFA.DAS.FAT.Domain.Shortlist.Api;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FAT.Application.UnitTests.Shortlist.Services
 {
     public class WhenGettingShortlistForUser
     {
-        
+        [Test, MoqAutoData]
+        public async Task Then_The_Data_Is_Returned_From_The_Api(
+            Guid shortlistUserId,
+            Domain.Shortlist.ShortlistForUser apiResponse,
+            [Frozen] Mock<IApiClient> apiClient,
+            ShortlistService service)
+        {
+            //Arrange
+            apiClient.Setup(x =>
+                    x.Get<Domain.Shortlist.ShortlistForUser>(
+                        It.Is<GetShortlistForUserApiRequest>(c => c.GetUrl.Contains(shortlistUserId.ToString(), StringComparison.InvariantCultureIgnoreCase))))
+                .ReturnsAsync(apiResponse);
+            
+            //Act
+            var actual = await service.GetShortlistForUser(shortlistUserId);
+            
+            //Assert
+            actual.Should().BeEquivalentTo(apiResponse);
+        }
     }
 }
---
+++ src/SFA.DAS.FAT.Application/Shortlist/Services/ShortlistService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using SFA.DAS.FAT.Domain.Configuration;
+using SFA.DAS.FAT.Domain.Interfaces;
+using SFA.DAS.FAT.Domain.Shortlist;
+using SFA.DAS.FAT.Domain.Shortlist.Api;
+
+namespace SFA.DAS.FAT.Application.Shortlist.Services
+{
+    public class ShortlistService
+    {
+        private readonly IApiClient _apiClient;
+        private readonly FindApprenticeshipTrainingApi _configValue;
+
+        public ShortlistService(IApiClient apiClient, IOptions<FindApprenticeshipTrainingApi> config)
+        {
+            _apiClient = apiClient;
+            _configValue = config.Value;
+        }
+
+        public async Task<ShortlistForUser> GetShortlistForUser(Guid shortlistUserId)
+        {
+            var apiRequest = new GetShortlistForUserApiRequest(_configValue.BaseUrl, shortlistUserId);
+
+            var apiResponse = await _apiClient.Get<ShortlistForUser>(apiRequest);
+
+            return apiResponse;
+        }
+    }
+}

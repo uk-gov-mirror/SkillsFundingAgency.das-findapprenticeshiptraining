313a51743c036b80b8a78afc3b1b610635579844
Scott Wakefield <scott@expertcc.co.uk>
2021-02-12 15:49:34 +0000

    FAT2-230 adding view model.

--- src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingShortlistViewModel.cs
+++ src/SFA.DAS.FAT.Web.UnitTests/Models/WhenCreatingShortlistViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace SFA.DAS.FAT.Web.UnitTests.Models
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Shortlist;
+using SFA.DAS.FAT.Web.Models;
+
+namespace SFA.DAS.FAT.Web.UnitTests.Models
 {
     public class WhenCreatingShortlistViewModel
     {
-        
+        [Test, AutoData]
+        public void Then_The_Values_Are_Mapped_Correctly(ShortlistItem source)
+        {
+            var actual = (ShortlistItemViewModel)source;
+            
+            actual.Should().BeEquivalentTo(source, options => options
+                .Excluding(item => item.ShortlistUserId)
+                .Excluding(item => item.Course)
+                .Excluding(item => item.Provider));
+            actual.Course.Should().BeEquivalentTo((CourseViewModel)source.Course);
+            actual.Provider.Should().BeEquivalentTo((ProviderViewModel)source.Provider);
+        }
     }
 }
---
+++ src/SFA.DAS.FAT.Web/Models/ShortlistViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.FAT.Domain.Shortlist;
+
+namespace SFA.DAS.FAT.Web.Models
+{
+    public class ShortlistViewModel
+    {
+        public IEnumerable<ShortlistItemViewModel> Shortlist { get; set; }
+    }
+
+    public class ShortlistItemViewModel
+    {
+        public Guid Id { get; set; }
+        public ProviderViewModel Provider { get; set; }
+        public CourseViewModel Course { get; set; }
+        public string LocationDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public static implicit operator ShortlistItemViewModel(ShortlistItem source)
+        {
+            return new ShortlistItemViewModel
+            {
+                Id = source.Id,
+                LocationDescription = source.LocationDescription,
+                CreatedDate = source.CreatedDate,
+                Course = source.Course,
+                Provider = source.Provider
+            };
+        }
+    }
+}

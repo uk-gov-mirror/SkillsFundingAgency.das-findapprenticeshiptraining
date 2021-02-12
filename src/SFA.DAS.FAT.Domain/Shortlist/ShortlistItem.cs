63acd1b0c0cd485681e825663aa8e3de8f7a0aa2
Scott Wakefield <scott@expertcc.co.uk>
2021-02-12 13:23:45 +0000

    FAT2-230 adding response models.

---
+++ src/SFA.DAS.FAT.Domain/Shortlist/ShortlistItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFA.DAS.FAT.Domain.Courses;
+
+namespace SFA.DAS.FAT.Domain.Shortlist
+{
+    public class ShortlistItem
+    {
+        public Guid Id { get; set; }
+        public Guid ShortlistUserId { get; set; }
+        public Provider Provider { get; set; }
+        public Course Course { get; set; }
+        public string LocationDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

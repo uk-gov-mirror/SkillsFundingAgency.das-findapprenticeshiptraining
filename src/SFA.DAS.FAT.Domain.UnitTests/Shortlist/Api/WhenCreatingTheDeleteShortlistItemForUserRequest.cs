df327e258a543df84a5c9e24990297c0d72b2b51
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-15 14:21:49 +0000

    Add delete shortlist item request

---
+++ src/SFA.DAS.FAT.Domain.UnitTests/Shortlist/Api/WhenCreatingTheDeleteShortlistItemForUserRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FAT.Domain.Shortlist.Api;
+
+namespace SFA.DAS.FAT.Domain.UnitTests.Shortlist.Api
+{
+    public class WhenCreatingTheDeleteShortlistItemForUserRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Request_Is_Built_Correctly(string baseUrl, Guid id, Guid shortlistUserid)
+        {
+            //Arrange Act
+            var actual = new DeleteShortlistForUserRequest(baseUrl, id, shortlistUserid);
+            
+            //Assert
+            actual.DeleteUrl.Should().Be($"{baseUrl}shortlist/users/{shortlistUserid}/items/{id}");
+        }
+    }
+}
---
+++ src/SFA.DAS.FAT.Domain/Shortlist/Api/DeleteShortlistForUserRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SFA.DAS.FAT.Domain.Interfaces;
+
+namespace SFA.DAS.FAT.Domain.Shortlist.Api
+{
+    public class DeleteShortlistForUserRequest : IDeleteApiRequest
+    {
+        private readonly Guid _id;
+        private readonly Guid _shortlistUserid;
+
+        public DeleteShortlistForUserRequest(string baseUrl, Guid id, Guid shortlistUserid)
+        {
+            _id = id;
+            _shortlistUserid = shortlistUserid;
+            BaseUrl = baseUrl;
+        }
+
+        public string BaseUrl { get; }
+        public string DeleteUrl => $"{BaseUrl}shortlist/users/{_shortlistUserid}/items/{_id}";
+    }
+}

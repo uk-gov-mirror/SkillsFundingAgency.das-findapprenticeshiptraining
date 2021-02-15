a9c4bd34888b4e434d3fbc84d4dcbe68302644cf
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-15 16:30:37 +0000

    Add delete shortlist item command handler

---
+++ src/SFA.DAS.FAT.Application/Shortlist/Commands/DeleteShortlistItemForUser/DeleteShortlistItemForUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace SFA.DAS.FAT.Application.Shortlist.Commands.DeleteShortlistItemForUser
+{
+    public class DeleteShortlistItemForUserCommand : IRequest<Unit>
+    {
+        public Guid Id { get ; set ; }
+        public Guid ShortlistUserId { get ; set ; }
+    }
+}
---
+++ src/SFA.DAS.FAT.Application/Shortlist/Commands/DeleteShortlistItemForUser/DeleteShortlistItemForUserCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.FAT.Domain.Interfaces;
+
+namespace SFA.DAS.FAT.Application.Shortlist.Commands.DeleteShortlistItemForUser
+{
+    public class DeleteShortlistItemForUserCommandHandler : IRequestHandler<DeleteShortlistItemForUserCommand, Unit>
+    {
+        private readonly IShortlistService _service;
+
+        public DeleteShortlistItemForUserCommandHandler (IShortlistService service)
+        {
+            _service = service;
+        }
+        public async Task<Unit> Handle(DeleteShortlistItemForUserCommand request, CancellationToken cancellationToken)
+        {
+            await _service.DeleteShortlistItemForUser(request.Id, request.ShortlistUserId);
+            
+            return Unit.Value;
+        }
+    }
+}

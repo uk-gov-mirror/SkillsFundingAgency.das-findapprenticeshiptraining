9d3ab1f4acb3faf85f6bf836dcef10fcee6127fc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-18 22:19:48 +0000

    Change URL to pass shortlistuser id

--- src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseProviderDetailsApiRequest.cs
+++ src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseProviderDetailsApiRequest.cs
@@ -10,15 +10,17 @@ namespace SFA.DAS.FAT.Domain.Courses.Api
     {
         private readonly int _providerId;
         private readonly string _location;
+        private readonly Guid _shortlistUserId;
         private readonly double _lat;
         private readonly double _lon;
 
         private readonly int _courseId;
 
-        public GetCourseProviderDetailsApiRequest(string baseUrl, int courseId, int providerId, string location, double lat = 0, double lon = 0)
+        public GetCourseProviderDetailsApiRequest(string baseUrl, int courseId, int providerId, string location, Guid shortlistUserId, double lat = 0, double lon = 0)
         {
             _providerId = providerId;
             _location = location;
+            _shortlistUserId = shortlistUserId;
             _lat = lat;
             _lon = lon;
             BaseUrl = baseUrl;
@@ -26,6 +28,6 @@ namespace SFA.DAS.FAT.Domain.Courses.Api
         }
 
         public string BaseUrl { get; }
-        public string GetUrl => $"{BaseUrl}trainingcourses/{_courseId}/providers/{_providerId}?location={HttpUtility.UrlEncode(_location)}&lat={_lat}&lon={_lon}";
+        public string GetUrl => $"{BaseUrl}trainingcourses/{_courseId}/providers/{_providerId}?location={HttpUtility.UrlEncode(_location)}&lat={_lat}&lon={_lon}&shortlistUserId={_shortlistUserId}";
     }
 }

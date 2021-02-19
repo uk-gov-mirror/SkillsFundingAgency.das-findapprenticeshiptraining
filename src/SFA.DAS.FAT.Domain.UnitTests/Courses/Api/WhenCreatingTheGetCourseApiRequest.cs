fc12069bd2e5345a28af11cce4429c232f2b5398
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-19 07:44:23 +0000

    Update get request to pass shortlistuser id

--- src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCourseApiRequest.cs
+++ src/SFA.DAS.FAT.Domain.UnitTests/Courses/Api/WhenCreatingTheGetCourseApiRequest.cs
@@ -1,3 +1,4 @@
+using System;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
@@ -14,7 +15,17 @@ namespace SFA.DAS.FAT.Domain.UnitTests.Courses.Api
             var actual = new GetCourseApiRequest(baseUrl, id,lat,lon);
             
             //Assert
-            actual.GetUrl.Should().Be($"{baseUrl}trainingcourses/{id}?lat={lat}&lon={lon}");
+            actual.GetUrl.Should().Be($"{baseUrl}trainingcourses/{id}?lat={lat}&lon={lon}&shortlistUserId=");
+        }
+        
+        [Test, AutoData]
+        public void Then_The_Get_Url_Is_Constructed_Correctly_With_ShortlistUserId(string baseUrl, int id, double lat, double lon, Guid shortlistUserId)
+        {
+            //Arrange Act
+            var actual = new GetCourseApiRequest(baseUrl, id,lat,lon, shortlistUserId);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"{baseUrl}trainingcourses/{id}?lat={lat}&lon={lon}&shortlistUserId={shortlistUserId}");
         }
     }
 }
--- src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseApiRequest.cs
+++ src/SFA.DAS.FAT.Domain/Courses/Api/GetCourseApiRequest.cs
@@ -1,3 +1,4 @@
+using System;
 using SFA.DAS.FAT.Domain.Interfaces;
 
 namespace SFA.DAS.FAT.Domain.Courses.Api
@@ -6,17 +7,19 @@ namespace SFA.DAS.FAT.Domain.Courses.Api
     {
         private readonly double _lat;
         private readonly double _lon;
+        private readonly Guid? _shortlistUserId;
 
-        public GetCourseApiRequest (string baseUrl, int id, double lat, double lon)
+        public GetCourseApiRequest (string baseUrl, int id, double lat, double lon, Guid? shortlistUserId = null)
         {
             _lat = lat;
             _lon = lon;
+            _shortlistUserId = shortlistUserId;
             BaseUrl = baseUrl;
             Id = id;
         }
 
         private int Id { get; }
         public string BaseUrl { get; }
-        public string GetUrl => $"{BaseUrl}trainingcourses/{Id}?lat={_lat}&lon={_lon}";
+        public string GetUrl => $"{BaseUrl}trainingcourses/{Id}?lat={_lat}&lon={_lon}&shortlistUserId={_shortlistUserId}";
     }
 }

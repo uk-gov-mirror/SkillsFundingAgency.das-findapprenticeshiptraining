34b0ea6ad30f522bcc6e6d7ed39882d2b0c58758
Scott Wakefield <scott@expertcc.co.uk>
2021-03-04 12:26:28 +0000

    AEDB-7 adding config for aed stuff.

--- src/SFA.DAS.FAT.Domain/Configuration/FindApprenticeshipTrainingWeb.cs
+++ src/SFA.DAS.FAT.Domain/Configuration/FindApprenticeshipTrainingWeb.cs
@@ -7,5 +7,7 @@
         public string ZendeskSectionId { get; set; }
         public string ZendeskSnippetKey { get; set; }
         public string ZendeskCoBrowsingSnippetKey { get; set; }
+        public bool EmployerDemandFeatureToggle { get; set; }
+        public string EmployerDemandUrl { get; set; }
     }
 }
